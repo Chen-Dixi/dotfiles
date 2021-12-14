7b7c31c9b63a2cf7c8c242afa1552b61ed3a4a05
Dixi Chen (FA Talent) <v-dixichen@microsoft.com>
2021-12-14 11:03:40 +0800

    udpate jupyter password

--- dockerfile/jupyter/jupyter_notebook_config.py
+++ dockerfile/jupyter/jupyter_notebook_config.py
@@ -1,5 +1,5 @@
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
-c.NotebookApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$wHMwHqsIwMFkhZSIUQrezg$EgwmKxOmU0C56iQVcVWobg'
+c.NotebookApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$aJBM0R1d5cFA2nZLXFguEw$TsQWifean2vKP2btaZqc/Q'
 c.NotebookApp.port = 8888
