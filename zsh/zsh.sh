9037ed41824e3bda1f60c3d7c0f7d83e08cd8f9a
Chen-Dixi <cdxcqu2014@163.com>
2022-01-03 22:29:16 +0800

    update zshrc

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -3,6 +3,7 @@ cp ./zshrc ~/.zshrc
 cp ./oh-my-zsh/themes/astro.zsh-theme ~/.oh-my-zsh/themes/astro.zsh-theme
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
+g++ --std=c++11 zsh_tools/RadixConverter.cpp -o zsh_tools/RadixConverter_exe
 cp -rf ./general_alias ~/.general_alias
 cp -rf ./software_alias ~/.software_alias
 cp -rf ./zsh_tools ~/.zsh_tools
