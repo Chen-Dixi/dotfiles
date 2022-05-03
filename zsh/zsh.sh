ec005480a3f8dd00bd553f94751925c9b7034a1b
Chen-Dixi <cdxcqu2014@163.com>
2022-05-03 17:01:08 +0800

    udpate docker file and zsh script

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,7 +1,7 @@
 cp ./zshrc ~/.zshrc
 
 cp ./oh-my-zsh/themes/astro.zsh-theme ~/.oh-my-zsh/themes/astro.zsh-theme
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://gitee.com/jklash1996/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 g++ --std=c++11 zsh_tools/RadixConverter.cpp -o zsh_tools/RadixConverter_exe
 cp -rf ./general_alias ~/.general_alias
